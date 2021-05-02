7b48eb268eb47532ebf0d2ee327cc3d648917430
pavelov2013 <pavelov2013@gmail.com>
2021-05-02 23:00:26 +0300

    1.0

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace FigureConstructor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FigureList = new System.Windows.Forms.ListBox();
+            this.newFile = new System.Windows.Forms.Button();
+            this.loadFile = new System.Windows.Forms.Button();
+            this.saveFile = new System.Windows.Forms.Button();
+            this.AddFigure = new System.Windows.Forms.Button();
+            this.RemoveFigure = new System.Windows.Forms.Button();
+            this.canvas = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SelectBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // FigureList
+            // 
+            this.FigureList.FormattingEnabled = true;
+            this.FigureList.Location = new System.Drawing.Point(450, 30);
+            this.FigureList.Name = "FigureList";
+            this.FigureList.Size = new System.Drawing.Size(180, 173);
+            this.FigureList.TabIndex = 1;
+            this.FigureList.SelectedIndexChanged += new System.EventHandler(this.FigureList_SelectedIndexChanged);
+            // 
+            // newFile
+            // 
+            this.newFile.Location = new System.Drawing.Point(30, 0);
+            this.newFile.Name = "newFile";
+            this.newFile.Size = new System.Drawing.Size(75, 23);
+            this.newFile.TabIndex = 2;
+            this.newFile.Text = "New";
+            this.newFile.UseVisualStyleBackColor = true;
+            this.newFile.Click += new System.EventHandler(this.newFile_Click);
+            // 
+            // loadFile
+            // 
+            this.loadFile.Location = new System.Drawing.Point(120, 0);
+            this.loadFile.Name = "loadFile";
+            this.loadFile.Size = new System.Drawing.Size(75, 23);
+            this.loadFile.TabIndex = 3;
+            this.loadFile.Text = "Load";
+            this.loadFile.UseVisualStyleBackColor = true;
+            this.loadFile.Click += new System.EventHandler(this.loadFile_Click);
+            // 
+            // saveFile
+            // 
+            this.saveFile.Location = new System.Drawing.Point(210, 0);
+            this.saveFile.Name = "saveFile";
+            this.saveFile.Size = new System.Drawing.Size(75, 23);
+            this.saveFile.TabIndex = 4;
+            this.saveFile.Text = "Save";
+            this.saveFile.UseVisualStyleBackColor = true;
+            this.saveFile.Click += new System.EventHandler(this.saveFile_Click);
+            // 
+            // AddFigure
+            // 
+            this.AddFigure.Location = new System.Drawing.Point(450, 210);
+            this.AddFigure.Name = "AddFigure";
+            this.AddFigure.Size = new System.Drawing.Size(75, 23);
+            this.AddFigure.TabIndex = 6;
+            this.AddFigure.Text = "Add";
+            this.AddFigure.UseVisualStyleBackColor = true;
+            this.AddFigure.Click += new System.EventHandler(this.AddFigure_Click);
+            // 
+            // RemoveFigure
+            // 
+            this.RemoveFigure.Location = new System.Drawing.Point(540, 210);
+            this.RemoveFigure.Name = "RemoveFigure";
+            this.RemoveFigure.Size = new System.Drawing.Size(75, 23);
+            this.RemoveFigure.TabIndex = 7;
+            this.RemoveFigure.Text = "Remove";
+            this.RemoveFigure.UseVisualStyleBackColor = true;
+            this.RemoveFigure.Click += new System.EventHandler(this.RemoveFigure_Click);
+            // 
+            // canvas
+            // 
+            this.canvas.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.canvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.canvas.Location = new System.Drawing.Point(30, 30);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(360, 360);
+            this.canvas.TabIndex = 8;
+            this.canvas.Paint += new System.Windows.Forms.PaintEventHandler(this.canvas_Paint);
+            this.canvas.MouseClick += new System.Windows.Forms.MouseEventHandler(this.canvas_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(450, 270);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Color:";
+            // 
+            // SelectBox
+            // 
+            this.SelectBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.SelectBox.FormattingEnabled = true;
+            this.SelectBox.Location = new System.Drawing.Point(450, 300);
+            this.SelectBox.Name = "SelectBox";
+            this.SelectBox.Size = new System.Drawing.Size(180, 21);
+            this.SelectBox.TabIndex = 10;
+            this.SelectBox.SelectedIndexChanged += new System.EventHandler(this.SelectBox_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(911, 574);
+            this.Controls.Add(this.SelectBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.canvas);
+            this.Controls.Add(this.RemoveFigure);
+            this.Controls.Add(this.AddFigure);
+            this.Controls.Add(this.saveFile);
+            this.Controls.Add(this.loadFile);
+            this.Controls.Add(this.newFile);
+            this.Controls.Add(this.FigureList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox FigureList;
+        private System.Windows.Forms.Button newFile;
+        private System.Windows.Forms.Button loadFile;
+        private System.Windows.Forms.Button saveFile;
+        private System.Windows.Forms.Button AddFigure;
+        private System.Windows.Forms.Button RemoveFigure;
+        private System.Windows.Forms.Panel canvas;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox SelectBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,348 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FigureConstructor
+{
+    public partial class Form1 : Form
+    {
+        private List<int[]> coords = new List<int[]>();
+        private List<int[]> FCoords = new List<int[]>();
+        private List<string> FigureColors = new List<string>();
+        int Active = 0;
+        public int count = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            string[] nm = Enum.GetNames(typeof(KnownColor));
+            foreach (string item in nm)
+            {
+                SelectBox.Items.Add(item);
+            }
+        }
+        private void CanUse(bool can)
+        {
+            AddFigure.Enabled = can;
+            RemoveFigure.Enabled = can;
+            saveFile.Enabled = can;
+            canvas.Enabled = can;
+            FigureList.Enabled = can;
+            loadFile.Enabled = !can;
+            SelectBox.Enabled = can;
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CanUse(false);
+        }
+
+        private void canvas_Click(object sender, MouseEventArgs e)
+        {
+            int Xclick = canvas.PointToClient(Cursor.Position).X;
+            int Yclick = canvas.PointToClient(Cursor.Position).Y;
+            if(e.Button == MouseButtons.Left)
+            {
+                   AddToCanv(Xclick,Yclick);
+
+            }
+        }
+        private void AddToCanv(int Xclick, int Yclick)
+        {
+            PictureBox pic = new PictureBox();
+            pic.BackColor = Color.FromName(FigureColors[Active]);
+            pic.Size = new Size(59, 59);
+            pic.Location = new Point(Xclick / 60 * 60, Yclick / 60 * 60);
+            pic.MouseClick += pictureBox1_MouseClick;
+            for (int i = 0; i < coords.Count(); i++)
+            {
+                if (coords[i][0] == pic.Location.X && coords[i][1] == pic.Location.Y)
+                {
+                    return;
+                }
+            }
+            canvas.Controls.Add(pic);
+            int[] toadd = { pic.Location.X, pic.Location.Y };
+            coords.Add(toadd);
+            count = coords.Count();
+            AutoSave();
+        }
+        private void RemoveFromCanv(int Xclick,int Yclick)
+        {
+            for (int i = 0; i < coords.Count; i++)
+            {
+                if (Xclick/60*60 == coords[i][0] && Yclick/60*60 == coords[i][1] && FCoords[Active].Length >= 2)
+                {
+                    canvas.Controls.RemoveAt(i);
+                    int[] addedThis = new int[coords[i].Length-2];
+                    coords.RemoveAt(i);
+                    AutoSave();
+                    return;
+                }
+            }
+            
+        }
+        private void canvas_Paint(object sender, PaintEventArgs e)
+        {
+            Pen pen = new Pen(Color.FromArgb(255, 0, 0, 0));
+            for (int i = 1; i < 6; i++)
+            {
+                e.Graphics.DrawLine(pen, 60*i, 0, 60*i, 360);
+                e.Graphics.DrawLine(pen, 0, 60*i, 360, 60 * i);
+            }
+        }
+        private void LoadFigures()
+        {
+            if (!File.Exists("figures.fc"))
+            {
+                ErrorLoad("File Not exists");
+                return;
+            }
+            List<string> data = new List<string>();
+            data = File.ReadLines("figures.fc").ToList();
+            List<string> GetColor = new List<string>();
+            List<int[]> FiguresCoords = new List<int[]>();
+            for (int i = 0; i < data.Count(); i++)
+            {
+                string[] addition = data[i].Split(new char[] { '/' });
+                if (addition.Length != 2)
+                {
+                    ErrorLoad("length not equals 2");
+                    return;
+                }
+                GetColor.Add(addition[0]);
+
+                string[] crds = addition[1].Split(new char[] { ';' });
+                int[] cords = new int[crds.Length];
+                for (int t = 0; t < crds.Length; t++)
+                {
+                    try
+                    {
+                        cords[t] = int.Parse(crds[t]);
+                    }
+                    catch (Exception)
+                    {
+
+                        ErrorLoad("not int coords");
+                        return;
+                    }
+                }
+
+                FiguresCoords.Add(cords);
+
+            }
+
+            for (int i = 0; i < FiguresCoords.Count(); i++)
+            {
+                if (FiguresCoords[i].Length % 2 != 0)
+                {
+                    ErrorLoad("Some coords is not pair. this %2 != 0");
+                    return;
+                }
+                if (FiguresCoords[i].Length > 36*2)
+                {
+                    ErrorLoad("Too many objects");
+                    return;
+                }
+            }
+            for (int i = 0; i < FiguresCoords.Count(); i++)
+            {
+                for (int q = 0; q < FiguresCoords[i].Length; q += 2)
+                {
+                    if (FiguresCoords[i][q] % 30 != 0 || FiguresCoords[i][q] < 60 || FiguresCoords[i][q] > 210)
+                    {
+                        ErrorLoad("Xcoord can't multiply of base size");
+                        return;
+                    }
+                }
+                for (int q = 1; q < FiguresCoords[i].Length; q += 2)
+                {
+                    if (FiguresCoords[i][q] > 150 || FiguresCoords[i][q] < 0)
+                    {
+                        ErrorLoad("Ycoord is not in range");
+                        return;
+                    }
+                }
+            }
+            string[] ColorTypes = Enum.GetNames(typeof(KnownColor));
+            for (int i = 0; i < GetColor.Count(); i++)
+            {
+                bool truly = false;
+                for (int q = 0; q < ColorTypes.Length; q++)
+                {
+
+                    if (GetColor[i] == ColorTypes[q])
+                    {
+                        truly = true;
+                        break;
+                    }
+                }
+                if (!truly)
+                {
+                    ErrorLoad("Color is not exist");
+                    return;
+                }
+            }
+            FCoords = FiguresCoords;
+            FigureColors = GetColor;
+            MessageBox.Show("Success");
+        }
+        public void ErrorLoad(string text)
+        {
+            MessageBox.Show("File corrupted \n Error: " + text);
+        }
+
+        private void loadFile_Click(object sender, EventArgs e)
+        {
+            FCoords.Clear();
+            coords.Clear();
+            FigureColors.Clear();
+            for (int i = 0; i < FigureList.Items.Count; i++)
+            {
+                FigureList.SelectedIndexChanged -= FigureList_SelectedIndexChanged;
+                FigureList.Items.RemoveAt(i);
+                FigureList.SelectedIndexChanged += FigureList_SelectedIndexChanged;
+            }
+            LoadFigures();
+            LoadToList();
+            CanUse(true);
+        }
+        private void LoadToList()
+        {
+            for (int i = 0; i < FCoords.Count(); i++)
+            {
+                FigureList.SelectedIndexChanged += FigureList_SelectedIndexChanged;
+                FigureList.Items.Add(i);
+                FigureList.SelectedIndexChanged -= FigureList_SelectedIndexChanged;
+            }
+            if (coords.Count != 0)
+            {
+                FigureList.SelectedIndexChanged += FigureList_SelectedIndexChanged;
+                FigureList.SetSelected(0, true);
+                FigureList.SelectedIndexChanged -= FigureList_SelectedIndexChanged;
+            }
+        }
+        private void newFile_Click(object sender, EventArgs e)
+        {
+            var f = File.Create("figures.fc");
+            f.Close();
+            MessageBox.Show("Success");
+            CanUse(true);
+        }
+
+        private void FigureList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            coords.Clear();
+            Active = FigureList.SelectedIndex;
+            canvas.Controls.Clear();
+            for (int i = 0; i < FCoords[Active].Length/2; i++)
+            {
+                PictureBox pic = new PictureBox();
+                pic.BackColor = Color.FromName(FigureColors[Active]);
+                pic.Size = new Size(59, 59);
+                pic.Location = new Point((FCoords[Active][2 * i]-60)*2, FCoords[Active][2*i+1]*2);
+                pic.MouseClick += pictureBox1_MouseClick;
+                int[] toadd = { (FCoords[Active][2 * i] - 60)*2, FCoords[Active][(2 * i) + 1] * 2 };
+                coords.Add(toadd);
+                canvas.Controls.Add(pic);
+            }
+            for (int i = 0; i < SelectBox.Items.Count; i++)
+            {
+                if (SelectBox.Items[i].ToString() == FigureColors[Active])
+                {
+                    SelectBox.SelectedIndex = i;
+                }
+            }
+        }
+        private void FigureList_SelectedIndexChanged()
+        {
+            FigureColors[Active] = SelectBox.Text;
+            coords.Clear();
+            Active = FigureList.SelectedIndex;
+            canvas.Controls.Clear();
+            for (int i = 0; i < FCoords[Active].Length / 2; i++)
+            {
+                PictureBox pic = new PictureBox();
+                pic.BackColor = Color.FromName(FigureColors[Active]);
+                pic.Size = new Size(59, 59);
+                pic.Location = new Point((FCoords[Active][2 * i] - 60) * 2, FCoords[Active][2 * i + 1] * 2);
+                pic.MouseClick += pictureBox1_MouseClick;
+                int[] toadd = { (FCoords[Active][2 * i] - 60) * 2, FCoords[Active][(2 * i) + 1] * 2 };
+                coords.Add(toadd);
+                canvas.Controls.Add(pic);
+            }
+        }
+        private void AddFigure_Click(object sender, EventArgs e)
+        {
+            FigureList.Items.Add(FigureList.Items.Count);
+            List<int> empty = new List<int>();
+            FCoords.Add(empty.ToArray());
+            FigureColors.Add("Blue");
+        }
+
+        private void RemoveFigure_Click(object sender, EventArgs e)
+        {
+            FigureList.SelectedIndexChanged -= FigureList_SelectedIndexChanged;
+            FigureList.Items.RemoveAt(Active);
+            FigureList.SelectedIndexChanged += FigureList_SelectedIndexChanged;
+            FCoords.RemoveAt(Active);
+            FigureColors.RemoveAt(Active);
+
+            Active = 0;
+        }
+        private void AutoSave()
+        {
+            if (FCoords.Count == 0)
+            {
+                MessageBox.Show("No Figures");
+                return;
+            }
+            int[] add = new int[coords.Count() * 2];
+            for (int i = 0; i < coords.Count(); i++)
+            {
+                add[2 * i] = coords[i][0] / 2 + 60;
+                add[(2 * i) + 1] = coords[i][1] / 2;
+            }
+            FCoords[Active] = add;
+            FigureColors[Active] = SelectBox.Text;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            int Xclick = canvas.PointToClient(Cursor.Position).X;
+            int Yclick = canvas.PointToClient(Cursor.Position).Y;
+            if (e.Button == MouseButtons.Right)
+            {
+                RemoveFromCanv(Xclick, Yclick);
+            }
+        }
+
+        private void saveFile_Click(object sender, EventArgs e)
+        {
+            string[] toAdd = new string[FCoords.Count];
+            for (int i = 0; i < FCoords.Count(); i++)
+            {
+                toAdd[i] = FigureColors[i] + "/";
+                for (int q = 0; q < FCoords[i].Length; q++)
+                {
+                    toAdd[i] += FCoords[i][q] + ";";
+                }
+                string withoutLast = toAdd[i].Substring(0, (toAdd[i].Length - 1));
+                toAdd[i] = withoutLast;
+            }
+                File.WriteAllLines("figures.fc",toAdd);
+            MessageBox.Show("Success");
+        }
+
+        private void SelectBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            FigureList_SelectedIndexChanged();
+        }
+    }
+}
